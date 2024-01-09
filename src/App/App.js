03ad88280ddd62f6e9888df8be859a9cf7e69d36
butlertree <140225441+butlertree@users.noreply.github.com>
2024-01-09 12:00:55 -0700

    render dog to page

--- src/App/App.js
+++ src/App/App.js
@@ -5,10 +5,81 @@ import './App.css';
 import Dogs from '../Dogs/Dogs';
 import DogDetails from '../DogDetails/DogDetails';
 import NotFound from '../NotFound/NotFound';
+import dummydata from '../data/data';
+
+
+
 
 function App() {
-  return 
-    
+
+
+  const dummydata = [
+    {
+    "breeds": [
+    {
+    "weight": {
+    "imperial": "6 - 13",
+    "metric": "3 - 6"
+    },
+    "height": {
+    "imperial": "9 - 11.5",
+    "metric": "23 - 29"
+    },
+    "id": 1,
+    "name": "Affenpinscher",
+    "bred_for": "Small rodent hunting, lapdog",
+    "breed_group": "Toy",
+    "life_span": "10 - 12 years",
+    "temperament": "Stubborn, Curious, Playful, Adventurous, Active, Fun-loving",
+    "origin": "Germany, France",
+    "reference_image_id": "BJa4kxc4X"
+    }
+    ],
+    "id": "BJa4kxc4X",
+    "url": "https://cdn2.thedogapi.com/images/BJa4kxc4X_1280.jpg",
+    "width": 1600,
+    "height": 1199
+    },
+    ]
+
+
+
+
+  const [error, setError] = useState('')
+  const [dogs, setDogs] = useState(dummydata);
+
+  // useEffect(() => {
+  //   fetch('https://api.thedogapi.com/v1/images/search?limit=100&api_key=live_00isfy9kzQCyFWWBludIQFj4g1pDwEoM87PH2PTVx8njhE7q1oEBDzg5lOhHq0QZ')
+  //     .then(response => response.json())
+  //     .then(data => setDogs(data))
+  //     .catch(error => setError(error.message))
+  // }, []);
+
+
+
+  
+  return (
+    <main className='App'>
+      <header className='app-header'>
+          <h1 className='big-heading'>Digital Dog Park</h1>
+      </header>
+      <section className='main-content'>
+      <Routes>
+        <Route
+          path="/"
+          element={<Dogs dogs={dogs}/>}
+        />
+        <Route
+          path="*"
+          element={<NotFound />}
+        />
+      </Routes>
+      </section>
+      {error && <h2>Something went wrong, please try again later!</h2>}
+   
+    </main>
+  );
 }
 
 export default App;
+    
---
+++ src/data/data.js
@@ -0,0 +1,525 @@
+const dummydata = [
+    {
+    "breeds": [
+    {
+    "weight": {
+    "imperial": "6 - 13",
+    "metric": "3 - 6"
+    },
+    "height": {
+    "imperial": "9 - 11.5",
+    "metric": "23 - 29"
+    },
+    "id": 1,
+    "name": "Affenpinscher",
+    "bred_for": "Small rodent hunting, lapdog",
+    "breed_group": "Toy",
+    "life_span": "10 - 12 years",
+    "temperament": "Stubborn, Curious, Playful, Adventurous, Active, Fun-loving",
+    "origin": "Germany, France",
+    "reference_image_id": "BJa4kxc4X"
+    }
+    ],
+    "id": "BJa4kxc4X",
+    "url": "https://cdn2.thedogapi.com/images/BJa4kxc4X_1280.jpg",
+    "width": 1600,
+    "height": 1199
+    },
+    {
+    "breeds": [
+    {
+    "weight": {
+    "imperial": "40 - 65",
+    "metric": "18 - 29"
+    },
+    "height": {
+    "imperial": "21 - 23",
+    "metric": "53 - 58"
+    },
+    "id": 4,
+    "name": "Airedale Terrier",
+    "bred_for": "Badger, otter hunting",
+    "breed_group": "Terrier",
+    "life_span": "10 - 13 years",
+    "temperament": "Outgoing, Friendly, Alert, Confident, Intelligent, Courageous",
+    "origin": "United Kingdom, England",
+    "reference_image_id": "1-7cgoZSh"
+    }
+    ],
+    "id": "SJZIJgqEX",
+    "url": "https://cdn2.thedogapi.com/images/SJZIJgqEX_1280.jpg",
+    "width": 333,
+    "height": 500
+    },
+    {
+    "breeds": [
+    {
+    "weight": {
+    "imperial": "11.5 - 15.5",
+    "metric": "5 - 7"
+    },
+    "height": {
+    "imperial": "11 - 16",
+    "metric": "28 - 41"
+    },
+    "id": 51,
+    "name": "Border Terrier",
+    "bred_for": "Fox bolting, ratting",
+    "breed_group": "Terrier",
+    "life_span": "12 - 14 years",
+    "temperament": "Fearless, Affectionate, Alert, Obedient, Intelligent, Even Tempered",
+    "reference_image_id": "HJOpge9Em"
+    }
+    ],
+    "id": "HJOpge9Em",
+    "url": "https://cdn2.thedogapi.com/images/HJOpge9Em_1280.jpg",
+    "width": 674,
+    "height": 450
+    },
+    {
+    "breeds": [
+    {
+    "weight": {
+    "imperial": "55 - 88",
+    "metric": "25 - 40"
+    },
+    "height": {
+    "imperial": "21.5 - 26.5",
+    "metric": "55 - 67"
+    },
+    "id": 57,
+    "name": "Bracco Italiano",
+    "bred_for": "Versatile gun dog",
+    "breed_group": "Sporting",
+    "life_span": "10 - 12 years",
+    "temperament": "Stubborn, Affectionate, Loyal, Playful, Companionable, Trainable",
+    "reference_image_id": "S13yZg5VQ"
+    }
+    ],
+    "id": "S13yZg5VQ",
+    "url": "https://cdn2.thedogapi.com/images/S13yZg5VQ_1280.jpg",
+    "width": 750,
+    "height": 505
+    },
+    {
+    "breeds": [
+    {
+    "weight": {
+    "imperial": "13 - 14",
+    "metric": "6 - 6"
+    },
+    "height": {
+    "imperial": "9 - 10",
+    "metric": "23 - 25"
+    },
+    "id": 65,
+    "name": "Cairn Terrier",
+    "bred_for": "Bolting of otter, foxes, other vermin",
+    "breed_group": "Terrier",
+    "life_span": "14 - 15 years",
+    "temperament": "Hardy, Fearless, Assertive, Gay, Intelligent, Active",
+    "reference_image_id": "Sk7Qbg9E7"
+    }
+    ],
+    "id": "Sk7Qbg9E7",
+    "url": "https://cdn2.thedogapi.com/images/Sk7Qbg9E7_1280.jpg",
+    "width": 1254,
+    "height": 836
+    },
+    {
+    "breeds": [
+    {
+    "weight": {
+    "imperial": "50 - 55",
+    "metric": "23 - 25"
+    },
+    "height": {
+    "imperial": "19 - 23",
+    "metric": "48 - 58"
+    },
+    "id": 92,
+    "name": "Dalmatian",
+    "bred_for": "Carriage dog - trot alongside carriages to protect the occupants from banditry or other interference",
+    "breed_group": "Non-Sporting",
+    "life_span": "10 - 13 years",
+    "temperament": "Outgoing, Friendly, Energetic, Playful, Sensitive, Intelligent, Active",
+    "reference_image_id": "SkJ3blcN7"
+    }
+    ],
+    "id": "SkJ3blcN7",
+    "url": "https://cdn2.thedogapi.com/images/SkJ3blcN7_1280.jpg",
+    "width": 1200,
+    "height": 800
+    },
+    {
+    "breeds": [
+    {
+    "weight": {
+    "imperial": "44 - 66",
+    "metric": "20 - 30"
+    },
+    "height": {
+    "imperial": "18 - 23",
+    "metric": "46 - 58"
+    },
+    "id": 102,
+    "name": "English Shepherd",
+    "bred_for": "Herding & guarding livestock, farm watch dog",
+    "breed_group": "Working",
+    "life_span": "10 - 13 years",
+    "temperament": "Kind, Energetic, Independent, Adaptable, Intelligent, Bossy",
+    "reference_image_id": "H1QyMe5EQ"
+    }
+    ],
+    "id": "H1QyMe5EQ",
+    "url": "https://cdn2.thedogapi.com/images/H1QyMe5EQ_1280.jpg",
+    "width": 1920,
+    "height": 1280
+    },
+    {
+    "breeds": [
+    {
+    "weight": {
+    "imperial": "45 - 80",
+    "metric": "20 - 36"
+    },
+    "height": {
+    "imperial": "23 - 27",
+    "metric": "58 - 69"
+    },
+    "id": 123,
+    "name": "Gordon Setter",
+    "bred_for": "Find and point gamebirds",
+    "breed_group": "Sporting",
+    "life_span": "10 - 12 years",
+    "temperament": "Fearless, Alert, Loyal, Confident, Gay, Eager",
+    "reference_image_id": "SJ5vzx5NX"
+    }
+    ],
+    "id": "SJ5vzx5NX",
+    "url": "https://cdn2.thedogapi.com/images/SJ5vzx5NX_1280.jpg",
+    "width": 622,
+    "height": 467
+    },
+    {
+    "breeds": [
+    {
+    "weight": {
+    "imperial": "20 - 40",
+    "metric": "9 - 18"
+    },
+    "height": {
+    "imperial": "13 - 18",
+    "metric": "33 - 46"
+    },
+    "id": 165,
+    "name": "Miniature American Shepherd",
+    "breed_group": "Herding",
+    "life_span": "12 - 15 years",
+    "temperament": "Energetic, Loyal, Intelligent, Trainable",
+    "reference_image_id": "BkHHQgcN7"
+    }
+    ],
+    "id": "BkHHQgcN7",
+    "url": "https://cdn2.thedogapi.com/images/BkHHQgcN7_1280.jpg",
+    "width": 920,
+    "height": 640
+    },
+    {
+    "breeds": [
+    {
+    "weight": {
+    "imperial": "11 - 12",
+    "metric": "5 - 5"
+    },
+    "height": {
+    "imperial": "10",
+    "metric": "25"
+    },
+    "id": 176,
+    "name": "Norwich Terrier",
+    "bred_for": "Ratting, fox bolting",
+    "breed_group": "Terrier",
+    "life_span": "12 - 15 years",
+    "temperament": "Hardy, Affectionate, Energetic, Sensitive, Intelligent",
+    "reference_image_id": "BkgKXlqE7"
+    }
+    ],
+    "id": "BkgKXlqE7",
+    "url": "https://cdn2.thedogapi.com/images/BkgKXlqE7_1280.jpg",
+    "width": 1140,
+    "height": 640
+    },
+    {
+    "breeds": [
+    {
+    "weight": {
+    "imperial": "35 - 50",
+    "metric": "16 - 23"
+    },
+    "height": {
+    "imperial": "17 - 21",
+    "metric": "43 - 53"
+    },
+    "id": 177,
+    "name": "Nova Scotia Duck Tolling Retriever",
+    "breed_group": "Sporting",
+    "life_span": "12 - 14 years",
+    "temperament": "Outgoing, Alert, Patient, Intelligent, Loving",
+    "reference_image_id": "SyYtQe5V7"
+    }
+    ],
+    "id": "SyYtQe5V7",
+    "url": "https://cdn2.thedogapi.com/images/SyYtQe5V7_1280.jpg",
+    "width": 630,
+    "height": 420
+    },
+    {
+    "breeds": [
+    {
+    "weight": {
+    "imperial": "18 - 33",
+    "metric": "8 - 15"
+    },
+    "height": {
+    "imperial": "15 - 18.5",
+    "metric": "38 - 47"
+    },
+    "id": 205,
+    "name": "Pumi",
+    "breed_group": "Herding",
+    "life_span": "13 - 15 years",
+    "temperament": "Lively, Reserved, Intelligent, Active, Protective, Vocal",
+    "reference_image_id": "SyRe4xcN7"
+    }
+    ],
+    "id": "SyRe4xcN7",
+    "url": "https://cdn2.thedogapi.com/images/SyRe4xcN7_1280.jpg",
+    "width": 695,
+    "height": 391
+    },
+    {
+    "breeds": [
+    {
+    "weight": {
+    "imperial": "30 - 40",
+    "metric": "14 - 18"
+    },
+    "height": {
+    "imperial": "16 - 18",
+    "metric": "41 - 46"
+    },
+    "id": 233,
+    "name": "Soft Coated Wheaten Terrier",
+    "bred_for": "Vermin hunting, guarding, all-around farm helper",
+    "breed_group": "Terrier",
+    "life_span": "12 - 15 years",
+    "temperament": "Affectionate, Spirited, Energetic, Playful, Intelligent, Faithful",
+    "reference_image_id": "HJHmix5NQ"
+    }
+    ],
+    "id": "HJHmix5NQ",
+    "url": "https://cdn2.thedogapi.com/images/HJHmix5NQ_1280.jpg",
+    "width": 1200,
+    "height": 800
+    },
+    {
+    "breeds": [
+    {
+    "weight": {
+    "imperial": "9 - 15",
+    "metric": "4 - 7"
+    },
+    "height": {
+    "imperial": "10",
+    "metric": "25"
+    },
+    "id": 245,
+    "name": "Tibetan Spaniel",
+    "breed_group": "Non-Sporting",
+    "life_span": "12 - 15 years",
+    "temperament": "Willful, Aloof, Assertive, Independent, Playful, Intelligent, Happy",
+    "reference_image_id": "Hyjcol947"
+    }
+    ],
+    "id": "Hyjcol947",
+    "url": "https://cdn2.thedogapi.com/images/Hyjcol947_1280.jpg",
+    "width": 1023,
+    "height": 825
+    },
+    {
+    "breeds": [
+    {
+    "weight": {
+    "imperial": "15 - 22",
+    "metric": "7 - 10"
+    },
+    "height": {
+    "imperial": "10 - 11",
+    "metric": "25 - 28"
+    },
+    "id": 256,
+    "name": "West Highland White Terrier",
+    "bred_for": "Fox, badger, vermin hunting",
+    "breed_group": "Terrier",
+    "life_span": "15 - 20 years",
+    "temperament": "Hardy, Friendly, Alert, Independent, Gay, Active, Courageous",
+    "reference_image_id": "Bkdx2g5Em"
+    }
+    ],
+    "id": "Bkdx2g5Em",
+    "url": "https://cdn2.thedogapi.com/images/Bkdx2g5Em_1280.jpg",
+    "width": 1065,
+    "height": 1032
+    },
+    {
+    "breeds": [
+    {
+    "weight": {
+    "imperial": "25 - 35",
+    "metric": "11 - 16"
+    },
+    "height": {
+    "imperial": "18 - 22",
+    "metric": "46 - 56"
+    },
+    "id": 257,
+    "name": "Whippet",
+    "bred_for": "Coursing, racing",
+    "breed_group": "Hound",
+    "life_span": "12 - 15 years",
+    "temperament": "Friendly, Affectionate, Lively, Gentle, Intelligent, Quiet",
+    "reference_image_id": "Hyv-ne94m"
+    }
+    ],
+    "id": "Hyv-ne94m",
+    "url": "https://cdn2.thedogapi.com/images/Hyv-ne94m_1280.jpg",
+    "width": 1600,
+    "height": 1071
+    },
+    {
+    "breeds": [],
+    "id": "Bk4phvp47",
+    "url": "https://cdn2.thedogapi.com/images/Bk4phvp47.gif",
+    "width": 450,
+    "height": 254
+    },
+    {
+    "breeds": [],
+    "id": "ryLA3P64Q",
+    "url": "https://cdn2.thedogapi.com/images/ryLA3P64Q.gif",
+    "width": 192,
+    "height": 127
+    },
+    {
+    "breeds": [],
+    "id": "BJZlTPpVX",
+    "url": "https://cdn2.thedogapi.com/images/BJZlTPpVX.gif",
+    "width": 450,
+    "height": 250
+    },
+    {
+    "breeds": [],
+    "id": "B1YDzppN7",
+    "url": "https://cdn2.thedogapi.com/images/B1YDzppN7.gif",
+    "width": 280,
+    "height": 280
+    },
+    {
+    "breeds": [],
+    "id": "HkqQQaT4m",
+    "url": "https://cdn2.thedogapi.com/images/HkqQQaT4m.gif",
+    "width": 450,
+    "height": 254
+    },
+    {
+    "breeds": [],
+    "id": "HytzV6aNm",
+    "url": "https://cdn2.thedogapi.com/images/HytzV6aNm.gif",
+    "width": 400,
+    "height": 225
+    },
+    {
+    "breeds": [],
+    "id": "SJY6N6aVQ",
+    "url": "https://cdn2.thedogapi.com/images/SJY6N6aVQ.gif",
+    "width": 499,
+    "height": 320
+    },
+    {
+    "breeds": [],
+    "id": "H1HKuu2HQ",
+    "url": "https://cdn2.thedogapi.com/images/H1HKuu2HQ_1280.jpg",
+    "width": 1024,
+    "height": 683
+    },
+    {
+    "breeds": [],
+    "id": "r1mfYO2S7",
+    "url": "https://cdn2.thedogapi.com/images/r1mfYO2S7_1280.jpg",
+    "width": 1024,
+    "height": 683
+    },
+    {
+    "breeds": [],
+    "id": "SyNQKd3HQ",
+    "url": "https://cdn2.thedogapi.com/images/SyNQKd3HQ_1280.jpg",
+    "width": 1024,
+    "height": 683
+    },
+    {
+    "breeds": [
+    {
+    "weight": {
+    "imperial": "90 - 120",
+    "metric": "41 - 54"
+    },
+    "height": {
+    "imperial": "28 - 34",
+    "metric": "71 - 86"
+    },
+    "id": 5,
+    "name": "Akbash Dog",
+    "bred_for": "Sheep guarding",
+    "breed_group": "Working",
+    "life_span": "10 - 12 years",
+    "temperament": "Loyal, Independent, Intelligent, Brave",
+    "origin": "",
+    "reference_image_id": "26pHT3Qk7"
+    }
+    ],
+    "id": "Asut5CUgQ",
+    "url": "https://cdn2.thedogapi.com/images/Asut5CUgQ.jpg",
+    "width": 542,
+    "height": 411
+    },
+    {
+    "breeds": [
+    {
+    "weight": {
+    "imperial": "20 - 40",
+    "metric": "9 - 18"
+    },
+    "height": {
+    "imperial": "15 - 19",
+    "metric": "38 - 48"
+    },
+    "id": 12,
+    "name": "American Eskimo Dog",
+    "country_code": "US",
+    "bred_for": "Circus performer",
+    "breed_group": "Non-Sporting",
+    "life_span": "12 - 15 years",
+    "temperament": "Friendly, Alert, Reserved, Intelligent, Protective",
+    "reference_image_id": "Bymjyec4m"
+    }
+    ],
+    "id": "EB8A5HQHX",
+    "url": "https://cdn2.thedogapi.com/images/EB8A5HQHX.jpg",
+    "width": 1920,
+    "height": 1631
+    }
+
+]
+
+export default dummydata;
